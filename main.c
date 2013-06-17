dd8ce77544b21efd0ca0183df581d69dd3dfafed
CheezeCake <emmanuel.nicolet@gmail.com>
2013-06-17 03:43:02 +0200

    quiet option

--- main.c
+++ main.c
@@ -1,22 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "grid.h"
 #include "solve.h"
+
+void usage(void);
 
 int main(int argc, char **argv)
 {
 	Grid grid;
 	int error;
+	int quiet = 0;
 
 	if(argc == 1)
 		error = load_grid(stdin, &grid);
 	else if(argc == 2)
-		error = load_from_file(argv[1], &grid);
-	else
 	{
-		fprintf(stderr, "usage: sdksolver [file]\n");
-		return 1;
+		if(strcmp(argv[1], "-q") == 0)
+		{
+			quiet = 1;
+			error = load_grid(stdin, &grid);
+		}
+		else
+			error = load_from_file(argv[1], &grid);
 	}
+	else if(argc == 3)
+	{
+		if(strcmp(argv[1], "-q") == 0)
+		{
+			quiet = 1;
+			error = load_from_file(argv[2], &grid);
+		}
+		else
+			usage();
+	}
+	else
+		usage();
 
 	if(error == BAD_FORMAT)
 	{
@@ -31,10 +50,16 @@ int main(int argc, char **argv)
 	
 	if(solve(&grid, 0))
 		print_grid(&grid);
-	else
+	else if(!quiet)
 		printf("impossible to solve.\n");
 
 	destroy_grid(&grid);
 
 	return 0;
+}
+
+void usage(void)
+{
+	fprintf(stderr, "usage: sdksolver [-q] [file]\n");
+	exit(1);
 }
